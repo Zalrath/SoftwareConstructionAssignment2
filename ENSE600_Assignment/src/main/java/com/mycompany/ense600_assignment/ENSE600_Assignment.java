af58aa34eed2b0c6f840215ea927cf341cd722df
Zalrath <megan.norman.nz@gmail.com>
2025-07-29 13:37:26 +1200

    Adding NetBeans Project

---
+++ ENSE600_Assignment/src/main/java/com/mycompany/ense600_assignment/ENSE600_Assignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ense600_assignment;
+
+/**
+ *
+ * @author megan
+ */
+public class ENSE600_Assignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
