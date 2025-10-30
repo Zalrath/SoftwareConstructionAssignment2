a2d135932acdfd11b7b19221b886c50c014a7474
Zalrath <megan.norman.nz@gmail.com>
2025-10-31 12:38:39 +1300

    CHECKPOINT: SAVED

--- ENSE600_Assignment_P2/src/Assignment2/UI/Screens/BudgetQuadrants/BudgetVsActualPanel.java
+++ ENSE600_Assignment_P2/src/Assignment2/UI/Screens/BudgetQuadrants/BudgetVsActualPanel.java
@@ -31,17 +31,74 @@ public class BudgetVsActualPanel extends JPanel
     }
     
     // ----- initialise ui ----- //
-    private void buildUI()
+    private void buildUI() 
     {
         setLayout(new BorderLayout());
+
         setBackground(palette.tileMediumDark);
         setBorder(BorderFactory.createLineBorder(palette.tileDark, 2));
 
         // header
-        JLabel header = new JLabel("Budget vs Actual", SwingConstants.CENTER);
-        header.setFont(Theme.TITLE_FONT.deriveFont(20f));
+        JLabel header = new JLabel("budget vs actual", SwingConstants.CENTER);
+        header.setFont(Theme.TITLE_FONT.deriveFont(28f));
         header.setForeground(palette.textLight);
-        header.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, palette.accent));
+        header.setPreferredSize(new Dimension(300, 45));
+        header.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, palette.tileDark));
+        add(header, BorderLayout.NORTH);
+        
+        // container
+        JPanel contentPanel = new JPanel(new BorderLayout(10, 0)); // 10px gap between panels
+        contentPanel.setOpaque(false);
+        add(contentPanel, BorderLayout.CENTER);
+
+        
+        // middle panel
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBackground(palette.tileDark);
+        mainPanel.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, palette.tileDark));
+
+        
+        // right panel
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        rightPanel.setBackground(palette.tileMediumDark);
+        rightPanel.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, palette.tileDark));
+        rightPanel.setPreferredSize(new Dimension(120, 0));
+
+        JPanel rightHeader = new JPanel(new BorderLayout());
+        rightHeader.setBackground(palette.accent);
+        rightHeader.setMaximumSize(new Dimension(Integer.MAX_VALUE, 28));
+        
+        JLabel summaryLabel = new JLabel("summary", SwingConstants.CENTER);
+        summaryLabel.setForeground(palette.textLight);
+        summaryLabel.setFont(Theme.TITLE_FONT.deriveFont(18f));
+        rightHeader.add(summaryLabel, BorderLayout.CENTER);
+        
+        
+      
+        
+        rightPanel.add(rightHeader);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
         // placeholder for graph
         JPanel barPlaceholder = new JPanel();
@@ -52,8 +109,8 @@ public class BudgetVsActualPanel extends JPanel
         placeholderLabel.setForeground(palette.textLight.darker());
         barPlaceholder.add(placeholderLabel);
 
-        add(header, BorderLayout.NORTH);
-        add(barPlaceholder, BorderLayout.CENTER);
+        contentPanel.add(mainPanel, BorderLayout.CENTER);
+        contentPanel.add(rightPanel, BorderLayout.EAST);
     }
 
     // ----- external refresh ----- //
